104b9d072488e29298509120aece582bc2f5537c
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-08-28 20:33:26 -0400

    refactor: cell access API

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,16 +2,22 @@ require_relative "playing_pieces"
 
 # Represents a single cell on the TicTacToe board
 class Cell
-  include PlayingPieces
+  attr_reader :value # no need to test #attr_reader
 
-  attr_accessor :value
-
+  # This constructor only sets instance variables. There's no need to test it.
   def initialize(value)
     @value = value
   end
 
+  # incoming query: test the return value
   def occupied?
-    playing_pieces = [NOUGHT, CROSS]
+    playing_pieces = [PlayingPieces::NOUGHT, PlayingPieces::CROSS]
     playing_pieces.include?(@value)
+  end
+
+  # incoming command: test the direct public side effects
+  # incoming query: test the return value
+  def update(new_value)
+    @value = new_value
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,8 +1,8 @@
 require_relative "../lib/cell"
 
 describe Cell do
-  describe "#occupied" do
-    context "when cell is occupied by a nought" do
+  describe "#occupied?" do
+    context "when occupied by a nought" do
       subject(:cell_nought) { described_class.new(PlayingPieces::NOUGHT) }
 
       it "returns true" do
@@ -10,7 +10,7 @@ describe Cell do
       end
     end
 
-    context "when cell is occupied by a cross" do
+    context "when occupied by a cross" do
       subject(:cell_cross) { described_class.new(PlayingPieces::CROSS) }
 
       it "returns true" do
@@ -18,11 +18,45 @@ describe Cell do
       end
     end
 
-    context "when cell is not occupied by a playing piece" do
+    context "when not occupied by a playing piece" do
       subject(:cell_available) { described_class.new(1) }
 
       it "returns false" do
         expect(cell_available.occupied?).to be(false)
+      end
+    end
+  end
+
+  describe "#update" do
+    context "when updating value from an integer to a nought" do
+      subject(:cell_nine) { described_class.new(9) }
+
+      let(:new_value) { PlayingPieces::NOUGHT }
+
+      it "updates value to a nought" do
+        cell_nine.update(new_value)
+        expect(cell_nine.value).to eq(new_value)
+      end
+
+      it "returns a nought" do
+        result = cell_nine.update(new_value)
+        expect(result).to eq(new_value)
+      end
+    end
+
+    context "when updating value from an integer to a cross" do
+      subject(:cell_one) { described_class.new(1) }
+
+      let(:new_value) { PlayingPieces::CROSS }
+
+      it "updates value to a cross" do
+        cell_one.update(new_value)
+        expect(cell_one.value).to eq(new_value)
+      end
+
+      it "returns a cross" do
+        result = cell_one.update(new_value)
+        expect(result).to eq(new_value)
       end
     end
   end
